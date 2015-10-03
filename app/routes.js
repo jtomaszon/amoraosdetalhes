9db19192e890d4a3927d91311527e1d74ac23abe
Javier Tomas Zon <jtomaszon@gmail.com>
2015-10-03 17:07:43 -0300

    Adding more Like button

--- app/routes.js
+++ app/routes.js
@@ -1,20 +1,5 @@
-var fs       = require('fs');
+var fs   = require('fs');
 var data = JSON.parse(fs.readFileSync(__dirname + '/../public/data.json', 'utf8'));
-
-Array.prototype.contains = function(k, callback) {
-    var self = this;
-    return (function check(i) {
-        if (i >= self.length) {
-            return callback(false);
-        }
-
-        if (self[i] === k) {
-            return callback(true);
-        }
-
-        return process.nextTick(check.bind(null, i+1));
-    }(0));
-}
 
 module.exports = function(app) {
 
