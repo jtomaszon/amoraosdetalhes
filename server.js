8e0eff7fc3b2178a6b4bc42f0a77abc2613e0108
Javier Tomas Zon <jtomaszon@gmail.com>
2015-10-03 19:29:22 -0300

    Adding compression to express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // set up ======================================================================
 var express  = require('express');
+var compress = require('compression');
 var app      = express();                               // create our app w/ express
 var mongoose = require('mongoose');                     // mongoose for mongodb
 var port     = process.env.PORT || 8082;                // set the port
@@ -16,7 +17,8 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT
 var data = JSON.parse(fs.readFileSync(__dirname + '/public/data.json', 'utf8'));
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
+app.use(morgan('prod'));                                        // log every request to the console
+app.use(compress()); 
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
