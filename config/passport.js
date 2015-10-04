d6f881973a6b0cde729374690d700026191f656c
Javier Tomas Zon <jtomaszon@gmail.com>
2015-10-04 15:04:12 -0300

    Applying remote changes from remote

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ module.exports = function(passport) {
 
   // used to deserialize the user
   passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
+    User.findOne(id, function(err, user) {
       done(err, user);
     });
   });
@@ -22,13 +22,18 @@ module.exports = function(passport) {
   passport.use(new FacebookStrategy({
     clientID        : configAuth.facebookAuth.clientID,
     clientSecret    : configAuth.facebookAuth.clientSecret,
-    callbackURL     : configAuth.facebookAuth.callbackURL
+    callbackURL     : configAuth.facebookAuth.callbackURL,
+    enableProof     : true,
+    profileFields   : ['id', 'name', 'gender', 'profileUrl', 'emails']
+
   },
 
   // facebook will send back the token and profile
   function(token, refreshToken, profile, done) {
+    console.log("TOKEN", token);
+    console.log("PROFILE", profile);
     process.nextTick(function() {
-      User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+      User.findOne({ 'id' : profile.id }, function(err, user) {
           if (err)
             return done(err);
 
@@ -37,15 +42,17 @@ module.exports = function(passport) {
           } else {
             var newUser            = new User();
 
-            newUser.facebook.id    = profile.id; // set the users facebook id                   
-            newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
-            newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-            newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+            newUser.id    = profile.id;
+            newUser.token = token;
+            newUser.name  = profile.name.givenName + ' ' + profile.name.familyName;
+            newUser.email = profile.emails[0].value;
+
 
             newUser.save(function(err) {
                 if (err)
                     throw err;
 
+                console.log("New User", newUser);
                 return done(null, newUser);
             });
           }
