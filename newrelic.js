0e4483c07cd3057ce06735b745cbe31ef5de881b
Javier Tomas Zon <jtomaszon@gmail.com>
2015-10-15 13:54:56 -0300

    Adding NewRelic

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 exports.config = {
   app_name: ['Amor aos Detalhes'],
-  license_key: process.env.NEWRELIC_APIKEY;
+  license_key: process.env.NEWRELIC_APIKEY,
   logging: {
     level: 'info'
   }
